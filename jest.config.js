2d30d9a9543422fedbf79bb1ae674d0595a19ec4
Ruileirei <angruiwenray@gmail.com>
2025-06-29 15:02:49 +0800

    update jest.config

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,9 @@ module.exports = {
   transformIgnorePatterns: [
   'node_modules/(?!(expo|@expo|expo-.*|firebase|@firebase/.*|@firebase|react-native|@react-native|@react-navigation|react-native-.*|@rneui)/)',
 ],
-
+  testEnvironmentOptions: {
+    node: '16'
+  },
   setupFiles: ['./jest.setup.js'],
 };
 
